2f5d9090aea3ffcb2c4661b40de926de2996fcf5
ROE99k <roneecarumba99@gmail.com>
2025-07-02 12:58:23 +0545

    hi new dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter> {/* ✅ CHANGED */}
+        <HashRouter>
+          {" "}
+          {/* ✅ CHANGED */}
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
